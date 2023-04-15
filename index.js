96666eeac651d26ea11f49910fea8ecffd8035a6
nari37 <116661106+nari37@users.noreply.github.com>
2023-04-15 20:49:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const addlist = document.getElementById("#circle")
+const backButton1 = document.getElementById("#back")
+function openpage(){
+    window.open("http://127.0.0.1:5500/page2.html", "_blank");
+}
+function openindex(){
+    window.open("http://127.0.0.1:5500/index.html", "_blank");
+}
+function openthree(){
+    window.open("http://127.0.0.1:5500/page3.html", "_blank")
+}
+function openfinal(){
+    window.open("http://127.0.0.1:5500/final.html", "_blank");
+}
+function goFirst(){
+    window.open("http://127.0.0.1:5500/index.html","_blank");
+}
